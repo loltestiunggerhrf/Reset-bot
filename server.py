18fda1e6da4f2f6a8c09ee88a5041401e0ea5b67
loltestiunggerhrf <nativeroleplayrblx@gmail.com>
2025-04-02 17:13:51 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
-from flask import Flask, request, jsonify
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/reset_hwid', methods=['POST'])
-def reset_hwid():
-    # Handle HWID reset logic
-    return jsonify({"status": "success", "message": "HWID reset successful!"})
+@app.route('/')
+def home():
+    return "P DIDDY LOVER OVER HERE, GUYS TAKE A PICTURE!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  # Use 8080 or your platform's default
-    app.run(host="0.0.0.0", port=port)  # Ensure Flask binds to port 8080
+    port = int(os.environ.get("PORT", 10000))  # Use the port provided by Render
+    app.run(host="0.0.0.0", port=port)
