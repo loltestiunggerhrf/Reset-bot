7ac58182817cd0b7a79a5127c5fd320388b69975
loltestiunggerhrf <nativeroleplayrblx@gmail.com>
2025-04-02 11:28:07 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == '__main__':
+    # Use the PORT environment variable provided by Render
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no PORT is provided
+    app.run(host='0.0.0.0', port=port)
