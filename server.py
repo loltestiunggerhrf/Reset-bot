ae6297bf2b9ee97857dcc8abbf6e29266fc9cc13
loltestiunggerhrf <nativeroleplayrblx@gmail.com>
2025-04-02 16:16:11 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import threading
 import discord
 from discord.ext import commands
 from flask import Flask
+import os
 
 # Flask App Setup
 app = Flask(__name__)
@@ -14,7 +15,10 @@ def run_flask():
     app.run(host='0.0.0.0', port=8080)
 
 # Discord Bot Setup
-bot = commands.Bot(command_prefix='/')
+intents = discord.Intents.default()  # Initialize intents, allowing your bot to receive events
+intents.message_content = True  # Make sure this is enabled if you need to listen to message content
+
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
